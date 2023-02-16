2e239c7802714fcaab2f397ef98e5775879bfbeb
stevebozos <stefbozos@hotmail.com>
2023-02-16 23:38:02 +0000

    - Added the nebula enemy - Created all variables for the enemy - Drawed enemy into the game scene - Animated Enemy

--- dasher.cpp
+++ dasher.cpp
@@ -26,6 +26,9 @@ int main(){
     // Initialize player's Velocity //
     int player_velocity{0};
 
+    // Initialize Enemy Nebula Velocity in (pixels/second)//
+    int enemyNebula_Velocity{-600}; 
+
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -37,10 +40,16 @@ int main(){
     // Initialize Player animation frame in the spritesheet "scarfy.png"
     int frame{};
 
+    // Initialize Enemy Nebula animation frame.
+    int enemyNebula_frame{};
+
     // The amount of time before we update the animation frame
     // We want to update the animation 12 times per second in order to achieve a smooth running animation.
-    const float update_time{1.0f / 12.0f};
-    float running_time{};
+    const float player_updateTime{1.0f / 12.0f};
+    const float enemyNebule_updateTime{1.0f/12.0f};
+
+    float player_runningTime{};
+    float enemyNebula_runningTime{};
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -48,8 +57,12 @@ int main(){
 
     // // // SPRITE INITIALIZATION  // // //
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     // Texture2D, Rectangle and Vector2 are "compount data types", which are a custom data type 
     // created by the raylib library. LoadTecture is a method of the Texture2D class
+
+    // // // PLAYER SPRITE // // //
     Texture2D player = LoadTexture("textures/scarfy.png");
 
     Rectangle playerRectangle; // The rectangle that contains the player sprite
@@ -73,6 +86,28 @@ int main(){
     playerPosition.y = window_height - playerRectangle.height; // Place the player at the bottom of the screen on the Y axis.
 
     
+    // // // ENEMY SPRITES // // //
+
+    // NEBULA
+    Texture2D enemyNebula = LoadTexture("textures/12_nebula_spritesheet.png"); // Loads the Nebula enemy spritesheet (8x8)
+
+    Rectangle enemyNebula_Rectangle{0.0, 0.0, enemyNebula.width/8, enemyNebula.height/8};
+
+    // // Dimensions of the sprite rectangle for every frame
+    // enemyNebula_Rectangle.height = enemyNebula.height/8;
+    // enemyNebula_Rectangle.width = enemyNebula.width/8;
+
+    // // Starting point of the enemy rectangle on the spritesheet.
+    // enemyNebula_Rectangle.x = 0;
+    // enemyNebula_Rectangle.y = 0;
+
+    Vector2 enemyNebula_Position{window_width, (window_height - enemyNebula_Rectangle.height)};
+
+    // // The starting position of the enemy nebula in the scene.
+    // // We want the enemy to appear outside of the player's sight after the right edge of the window.
+    // enemyNebula_Position.x = window_width;
+    // enemyNebula_Position.y = window_height - enemyNebula_Rectangle.height;
+
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -119,14 +154,16 @@ int main(){
 
         /////////////////////////////////////////////////////////////////////
 
+        // Update the enemy nebula position on the X axis //
+        enemyNebula_Position.x += enemyNebula_Velocity * delta_time;
 
         // Update the player's velocity by 10 every frame on the Y axis //
         playerPosition.y += player_velocity * delta_time;
 
 
-        // // PLAYER ANIMATION // //
+        // // ANIMATION // //
         //////////////////////////////////////////////////////////////////////
-        // Update player's frame to create animation //
+
 
         // ***General formula for updating animation from a spritesheet***
         // x = frame * width/n
@@ -135,26 +172,52 @@ int main(){
         // width -> the spritesheet's width
         // n -> the number of animations on the spritesheet
 
-        // Update running time
-        running_time += delta_time;
-        
-        // Check if running time has surpassed update time
-        if(running_time >= update_time)
+        // We want to freeze the animation while the player is in air
+        // so the player does not "run" while jumping.
+        if(!isInAir)
         {
-            running_time = 0.0f;
+            // Update running time
+            player_runningTime += delta_time;
+            enemyNebula_runningTime += delta_time;
 
-            playerRectangle.x = frame * playerRectangle.width;
-            frame++; // Go to the next frame
-
-            // Reset the frame to avoid exceeding the spritesheet's width
-            if(frame > 5)
+            //Update the player's animation
+            // Check if running time has surpassed update time
+            if(player_runningTime >= player_updateTime)
             {
-                frame = 0;
+                player_runningTime = 0.0f;
+
+                playerRectangle.x = frame * playerRectangle.width;
+                frame++; // Go to the next frame
+            
+
+                // Reset the frame to avoid exceeding the spritesheet's width
+                if(frame > 5)
+                {
+                    frame = 0;
+                }
+            }
+
+            // Update the Enemy Nebula Animation
+            if(enemyNebula_runningTime >= enemyNebule_updateTime)
+            {
+                enemyNebula_runningTime = 0.0f;
+
+                enemyNebula_Rectangle.x = enemyNebula_frame * enemyNebula_Rectangle.width;
+                enemyNebula_frame++;
+
+                if(enemyNebula_frame > 7)
+                {
+                    enemyNebula_frame = 0;
+                }
             }
         }
 
+        
+
         ///////////////////////////////////////////////////////////////////////
 
+        // Draw the enemy Nebula //
+        DrawTextureRec(enemyNebula, enemyNebula_Rectangle, enemyNebula_Position, WHITE);
         
         // Draw the player on the screen //
         DrawTextureRec(player, playerRectangle, playerPosition, WHITE);
@@ -171,7 +234,7 @@ int main(){
     // Everytime in Raylib we load a texture at the start of the program,
     // we need to unload the texture at the end of the program.
     UnloadTexture(player);
-
+    UnloadTexture(enemyNebula);
     // Close the window that we have initialized before the program ends //
     CloseWindow();
 
